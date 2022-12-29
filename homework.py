3827bd016b11b08ca672fa6a017cc5697ebb00d9
Maxim Kononenko <maxim.kononenko@sim-labs.com>
2022-12-29 22:31:03 +0300

    Homework v0.0.7

--- homework.py
+++ homework.py
@@ -14,9 +14,9 @@ class InfoMessage:
         self.calories: float = calories
 
     def get_message(self) -> str:
-        message: str = f'Тип тренировки: {self.training_type};' + \
+        message: str = f'Тип тренировки: {self.training_type}; ' + \
             f'Длительность: {self.duration:.3f} ч.; ' + \
-            f'Дистанция: {self.distance:.3f} км;' + \
+            f'Дистанция: {self.distance:.3f} км; ' + \
             f'Ср. скорость: {self.speed:.3f} км/ч; ' + \
             f'Потрачено ккал: {self.calories:.3f}.'
 
